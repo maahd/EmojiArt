b679b64edd34168f465d97cc7369fe01e1248f17
maahd <maahdsh@gmail.com>
2019-09-27 20:07:27 +0200

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Maahd Shahzad on 9/27/19.
+//  Copyright © 2019 Maahd Shahzad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
