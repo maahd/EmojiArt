2f9ab2401c75c027ee4b5d7e8c00b2ee1f271a2f
maahd <maahdsh@gmail.com>
2019-09-30 15:24:06 +0200

    Drag and drop Table view Collection view

---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Maahd Shahzad on 9/29/19.
+//  Copyright © 2019 Maahd Shahzad. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var label: UILabel!
+}
